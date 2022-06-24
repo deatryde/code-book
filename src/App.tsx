5b4fd29c35eea4b3bd077893f52f927fdbd0cd2c
Rahul Dogra <rahuldogra1998@gmail.com>
2022-06-25 05:07:58 +0530

    :art: Improve: Structure

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+
+function getErrorMessage(error: unknown) {
+  if (error instanceof Error) return error.message;
+  return String(error);
+}
+
+function App() {
+  const initializeRef = useRef(false);
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const [error, setError] = useState<string | null>(null);
+
+  const startService = async () => {
+    if (initializeRef.current) return;
+
+    esbuild
+      .initialize({
+        worker: true,
+        wasmURL: '/esbuild.wasm',
+      })
+      .then(() => (initializeRef.current = true));
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!initializeRef.current) return;
+
+    try {
+      const result = await esbuild.transform(input, {
+        loader: 'jsx',
+        target: 'es2015',
+      });
+
+      setCode(result.code);
+    } catch (error) {
+      setError(getErrorMessage(error));
+    }
+  };
+
+  return (
+    <>
+      <textarea
+        cols={50}
+        rows={20}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+      <pre>{error}</pre>
+    </>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,6 @@
-import * as esbuild from "esbuild-wasm";
-import ReactDOM from "react-dom";
-import { useState, useEffect, useRef } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-function App() {
-  const ref = useRef<any>();
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-
-  const startService = async () => {
-    await esbuild.initialize({
-      worker: true,
-      wasmURL: "/esbuild.wasm",
-    });
-    ref.current = true;
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const onClick = () => {
-    if (!ref.current) {
-      return;
-    }
-
-    esbuild
-      .transform(input, {
-        loader: "jsx",
-        target: "es2015",
-      })
-      .then((result) => {
-        setCode(result.code);
-      });
-  };
-
-  return (
-    <>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <pre>{code}</pre>
-    </>
-  );
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
